9bbb11cc1284fd880cd7e6d9657c8588ebf73c3e
Joshua Hannaford <hannaford.joshua55@gmail.com>
2024-07-21 18:56:20 -0400

    Add ExpressionStatement

---
+++ Sources/Parser/expressionStatement.swift
@@ -0,0 +1,19 @@
+import Lexer
+
+public struct ExpressionStatement: Node {
+  // The first token of the expression
+  public let token: Token
+
+  // The expression that this statement represents
+  public let expression: Expression?
+
+  public func tokenLiteral() -> String {
+    return token.literal
+  }
+}
+
+extension ExpressionStatement: CustomStringConvertible {
+  public var description: String {
+    expression.map { $0.description } ?? ""
+  }
+}
