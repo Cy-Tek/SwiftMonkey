e003c08e9e7546e362d50beeb12f8e8c36d3a2cc
Joshua Hannaford <hannaford.joshua55@gmail.com>
2024-09-29 08:52:10 -0400

    feat: Add support for evaluating infix expressions
    This commit adds support for evaluating infix expressions in the Evaluator module. It introduces a new function `evalInfixExpression` that takes in a left operand, operator, and right operand, and evaluates the expression accordingly. The function is currently throwing an `EvaluationError.unimplemented` error, indicating that the implementation is not yet complete.
    
    The code changes include adding a new case in the `eval` function to handle `InfixExpression` nodes, extracting the left and right expressions, evaluating them, and calling the `evalInfixExpression` function. Additionally, the `evalNegationOperator` function has been added to handle the negation operator ("-") in prefix expressions.
    
    These changes are necessary to support more complex expressions and enhance the functionality of the Evaluator module.

--- Sources/Evaluator/evaluator.swift
+++ Sources/Evaluator/evaluator.swift
@@ -22,6 +22,19 @@ public func eval(node: Node) throws -> Object {
 
     let right = try eval(node: expr)
     return try evalPrefixExpression(op: pe.op, right: right)
+  case let ie as InfixExpression:
+    guard let leftExpr = ie.left else {
+      throw EvaluationError.evaluationFailed
+    }
+
+    guard let rightExpr = ie.right else {
+      throw EvaluationError.evaluationFailed
+    }
+
+    let leftValue = try eval(node: leftExpr)
+    let rightValue = try eval(node: rightExpr)
+
+    return try evalInfixExpression(left: leftValue, op: ie.op, right: rightValue)
   case let il as IntegerLiteral:
     return Integer(il.value)
   case let bl as BooleanLiteral:
@@ -50,9 +63,15 @@ func evalPrefixExpression(op: String, right: Object) throws -> Object {
   switch op {
   case "!":
     return try evalBangOperator(right: right)
+  case "-":
+    return try evalNegationOperator(right: right)
   default:
     throw EvaluationError.unimplemented
   }
+}
+
+func evalInfixExpression(left: Object, op: String, right: Object) throws -> Object {
+  throw EvaluationError.unimplemented
 }
 
 func evalBangOperator(right: Object) throws -> Object {
@@ -66,12 +85,20 @@ func evalBangOperator(right: Object) throws -> Object {
   default:
     return Boolean(false)
   }
+}
 
-  func evalNegationOperator(right: Object) throws -> Object {
-    guard let expr = right as? Integer else {
-      return Null()
-    }
-
-    return Integer(-expr.value)
+func evalNegationOperator(right: Object) throws -> Object {
+  guard let expr = right as? Integer else {
+    return Null()
   }
+
+  return Integer(-expr.value)
+}
+
+func evalPlusOperator(left: Object, right: Object) throws -> Object {
+  guard let leftInt = left as? Integer, let rightInt = right as? Integer else {
+    throw EvaluationError.evaluationFailed
+  }
+
+  return Integer(leftInt.value + rightInt.value)
 }
